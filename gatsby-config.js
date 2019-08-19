406edde36a846cbc3b60dabfbd76142a1bd20a14
gregleeper <gregleeper19@gmail.com>
2019-08-19 11:59:46 -0500

    anonymizer false

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-137475978-2',
-        anonymize: true,
+        anonymize: false,
         respectDNT: true,
       },
     },
